be409efe1c2ee1e24ad83945ee8c0f8dc3a959fe
zeittresor <35638136+zeittresor@users.noreply.github.com>
2024-09-25 00:10:26 +0200

    Update Test_Prompts.py
    dragon prompt changed, added another prompt

--- scripts/Test_Prompts.py
+++ scripts/Test_Prompts.py
@@ -12,26 +12,26 @@ class Script(scripts.Script):
         pose1 = gr.Checkbox(label="Fruits on a table", value=True)
         pose2 = gr.Checkbox(label="Majestic landscape", value=False)
         pose3 = gr.Checkbox(label="Modern cityscape at night", value=False)
-        pose4 = gr.Checkbox(label="Fantasy Dragon", value=False)
+        pose4 = gr.Checkbox(label="Evil Dragon", value=False)
         pose5 = gr.Checkbox(label="Posing woman", value=False)
-        pose6 = gr.Checkbox(label="Posing man", value=False)
+        pose6 = gr.Checkbox(label="Posing man", value=True)
+        pose7 = gr.Checkbox(label="Random Location", value=False)
 
-
-        prompt_group = [pose1, pose2, pose3, pose4, pose5, pose6]
+        prompt_group = [pose1, pose2, pose3, pose4, pose5, pose6, pose7]
 	
         with gr.Box():
 		      
             options_header = gr.Markdown("<br>Options")
             nsfw = gr.Checkbox(label="NSFW content", value=False)
             sfw = gr.Checkbox(label="SFW content", value=True)
-
+            
         return prompt_group + [nsfw, sfw]
 
-    def run(self, p, pose1, pose2, pose3, pose4, pose5, pose6, nsfw, sfw):
+    def run(self, p, pose1, pose2, pose3, pose4, pose5, pose6, pose7, nsfw, sfw):
 
         prompts = []
         negative_prompts = []
-
+        
         if pose1:
             prompts.append("photo of fruits on top of a table")
             negative_prompts.append("(drawing, painting, deformed, mutated, drawing, anime, ugly, worse photo, worse proportions)")
@@ -45,8 +45,8 @@ class Script(scripts.Script):
             negative_prompts.append("(blurry, overexposed, underexposed, low resolution, painting, drawing, anime, empty streets)")
 
         if pose4:
-            prompts.append("a realistic evil dragon is flying over a castle")
-            negative_prompts.append("(low resolution, blurry, overexposed, underexposed, cartoonish, unrealistic, anime, painting)")
+            prompts.append("a dangerous looking evil dragon is in a close distance in front of a natural cataclysm")
+            negative_prompts.append("(low resolution, blurry, overexposed, underexposed, cartoonish, rendering, 3dcg, uncanny valley, manga, comic, painting, wrong looking bodyparts, deformed, stupid)")
 
         if pose5:
             prompts.append("full-body photo of a posing beautiful woman in a photoshooting")
@@ -55,14 +55,18 @@ class Script(scripts.Script):
         if pose6:
             prompts.append("full-body photo with a posing male man in a photoshooting")
             negative_prompts.append("(worse bodyshape, deformations, mutations, painting, cartoon, any worse human-body, anime, invisible head, invisible body)")
-
+            
+        if pose7:
+            prompts.append("at a {outdoor hotel enviroment|castle tower roof|wintergarden with lots of windows|wildlife nationalpark|acient egyptian temple near a pyramide|random interesting location}")
+            negative_prompts.append("human mutations, human deformations, bad combination of told prompts, worse proportions")
+            
         if nsfw:
-            prompts.append("naughty, hot erotic, somehow sexual, explicit NSFW content")
-            negative_prompts.append("bad faces, worse, blurry")
+            prompts.append("naughty, nudity, NSFW")
+            negative_prompts.append("bad faces, fat, lowres, poorly drawn, worse, blurry, sfw")
 
         if sfw:
             prompts.append("sfw")
-            negative_prompts.append("porn, erotic, nudity, nsfw, naked")
+            negative_prompts.append("porn, erotic, lowres, poorly drawn, nudity, nsfw, nudity")
             
         p.prompt = ", ".join(prompts)
         p.negative_prompt = ", ".join(negative_prompts)
